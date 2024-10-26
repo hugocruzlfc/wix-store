c69ec728a14afa3220ae50a9df4c4a635ee45d58
Hugo Cruz <hugocruzlfc@gmail.com>
2024-10-26 18:01:51 +0200

    feat: add login and members

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,19 @@
 import brand from "@/assets/brand.png";
 import { getWixServerClient } from "@/lib/wix-client.server";
 import { getCart } from "@/wix-api/cart";
+import { getLoggedInMember } from "@/wix-api/members";
 import Image from "next/image";
 import Link from "next/link";
 import ShoppingCartButton from "./buttons/ShopingCartButon";
+import UserButton from "./buttons/UserButton";
 
 export default async function Navbar() {
-  const cart = await getCart(await getWixServerClient());
+  const wixClient = await getWixServerClient();
+
+  const [cart, loggedInMember] = await Promise.all([
+    getCart(wixClient),
+    getLoggedInMember(wixClient),
+  ]);
 
   return (
     <header className="bg-background shadow-sm">
@@ -15,7 +22,10 @@ export default async function Navbar() {
           <Image src={brand} alt="Flow Shop logo" width={150} height={50} />
           {/* <span className="text-xl font-bold">Flow Shop</span> */}
         </Link>
-        <ShoppingCartButton initialData={cart} />
+        <div className="flex items-center justify-center gap-5">
+          <UserButton loggedInMember={loggedInMember} />
+          <ShoppingCartButton initialData={cart} />
+        </div>
       </div>
     </header>
   );
